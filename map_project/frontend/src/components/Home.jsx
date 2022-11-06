d3714d8436a2313e77469bf07b2b910c3b902270
Amirreza <aazam.amirreza@gmail.com>
2022-11-06 01:24:23 -0500

    main page edit

--- map_project/frontend/src/components/Home.jsx
+++ map_project/frontend/src/components/Home.jsx
@@ -6,26 +6,38 @@ import styled from 'styled-components';
 
 const HomeWrapper = styled.div`
   margin: min(20rem, 30vh) 0;
+  /* background-image: linear-gradient(to bottom, rgb(96, 114, 179), rgb(145, 95, 149)); */
+
 `;
 
-const RedText = styled.span`
-  color: #8B0000;
-`;
+// const RedText = styled.span`
+//   color: #8B0000;
+// `;
 
-const GreenText = styled.span`
-  color: #006400;
+const whiteText = styled.span`
+  color: #eff8ef;
 `;
 
 const GoldText = styled.span`
-  color: #8B4513;
+  color: #f0de18;
+`;
+
+const TitleLine1 = styled.span`
+
+  text-decoration: solid;
+  text-align: center;
+  font-size: large;
+  border-radius: 50px;
+  color: #000000;
+  background-color: #bdcf22;
 `;
 
 const Home = () => {
   return (
     <HomeWrapper>
-      <Title><RedText>Alert</RedText> yourself on potential <GreenText>TTC</GreenText>/<GoldText>Bus</GoldText> accidents.</Title>
+      <Title><GoldText>Alert yourself on potential <whiteText>TTC</whiteText> Bus accidents.</GoldText></Title>
       <Subtitle>TrackTC will take care of all accidents in Downtown Toronto along your commute, using reliable database system.</Subtitle>
-      <Input>Enter your email address</Input>
+      {/* <Input>Enter your email address</Input> */}
       <Table/>
     </HomeWrapper>
   );
