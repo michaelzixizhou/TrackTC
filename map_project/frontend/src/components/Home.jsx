5c50c60e7285fa8806d0a6b55524b93a06958474
skim1601 <top030216@gmail.com>
2022-11-06 00:41:00 -0400

    frontend milestone A

--- map_project/frontend/src/components/Home.jsx
+++ map_project/frontend/src/components/Home.jsx
@@ -8,10 +8,22 @@ const HomeWrapper = styled.div`
   margin: min(20rem, 30vh) 0;
 `;
 
+const RedText = styled.span`
+  color: #8B0000;
+`;
+
+const GreenText = styled.span`
+  color: #006400;
+`;
+
+const GoldText = styled.span`
+  color: #8B4513;
+`;
+
 const Home = () => {
   return (
     <HomeWrapper>
-      <Title>Alert yourself on potential TTC/Bus accidents.</Title>
+      <Title><RedText>Alert</RedText> yourself on potential <GreenText>TTC</GreenText>/<GoldText>Bus</GoldText> accidents.</Title>
       <Subtitle>TrackTC will take care of all accidents in Downtown Toronto along your commute, using reliable database system.</Subtitle>
       <Input>Enter your email address</Input>
       <Table/>
